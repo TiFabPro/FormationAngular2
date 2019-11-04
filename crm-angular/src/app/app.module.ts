2275470f285639e941cd38a25fa91e691bfc76ef
Fabien LE-BOYER 755 <fabien.le-boyer@cnaf.fr>
2019-11-04 15:23:06 +0100

    Mon 1er message d'acceuil

--- crm-angular/src/app/app.module.ts
+++ crm-angular/src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Import des module
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Import necessaire au demarrage de l'appplication
+    LoginModule,
+    UiModule,
     BrowserModule
   ],
   providers: [],
