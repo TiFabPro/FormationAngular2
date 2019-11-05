c0202f86d4b2e4c0b4ec3e0e14affda4d108640e
Fabien LE-BOYER 755 <fabien.le-boyer@cnaf.fr>
2019-11-05 10:18:56 +0100

    ajout bootstrap, fontawesome, import Ui module

--- crm-angular/src/app/app.module.ts
+++ crm-angular/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-// Import des module
+// Import des modules
 import { LoginModule } from './login/login.module';
 import { UiModule } from './ui/ui.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { UiModule } from './ui/ui.module';
     // Import necessaire au demarrage de l'appplication
     LoginModule,
     UiModule,
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- crm-angular/src/app/ui/ui.module.ts
+++ crm-angular/src/app/ui/ui.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { UiComponent } from './containers/ui/ui.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import {CoderbaseUiModule} from '@coderbase/ui';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UiComponent, HeaderComponent, FooterComponent, NavComponent],
+  // Permet d'exporter le composant à l'exterieur
+  exports: [UiComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CoderbaseUiModule
   ]
 })
 export class UiModule { }
